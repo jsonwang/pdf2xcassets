27ff07b87e0b354b1b87acae679e3bdab14a1da5
jsonwang <287971051@qq.com>
2016-03-19 18:29:24 +0800

    v0.1.0

---
+++ pdfConvertXcassets/pdfConvertXcassets/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  pdfConvertXcassets
+//
+//  Created by AK on 16/3/19.
+//  Copyright © 2016年 ak. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
